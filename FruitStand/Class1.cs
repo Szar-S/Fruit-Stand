8e7646e4d4ee432beda9e89efbdaa7d76ccd9732
Szar-S <sezaikelekcier21@gmail.com>
2025-01-23 20:57:45 +0300

    update

---
+++ FruitStand/Class1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public abstract class Fruit
+{
+    public string Name;
+    public float Value;
+    public override string ToString()
+    {
+        return "";
+    }
+}
+public class Orange : Fruit
+{
+    public Orange(float v)
+    {
+        this.Name = "Orange";
+        this.Value = v;
+    }
+    public override string ToString()
+    {
+        return Name;
+    }
+
+}
+public class Banana : Fruit
+{
+    public Banana(int v)
+    {
+        this.Name = "Banana";
+        this.Value = v;
+    }
+    public override string ToString()
+    {
+        return Name;
+    }
+}
+public class Apple : Fruit
+{
+    public Apple(int v)
+    {
+        this.Name = "Apple";
+        this.Value = v;
+    }
+    public override string ToString()
+    {
+        return Name;
+    }
+}
+[Serializable]
+
+public class Basket
+{
+    public Fruit Fruit;
+    public int Quantity;
+    public Basket()
+    {
+        Fruit f;
+        Quantity = 0;
+    }
+    public Basket(Fruit f, int q)
+    {
+        this.Fruit = f;
+        this.Price = f.Value;
+        this.Quantity = q;
+    }
+    private float Price;
+    public double TotalPrice;
+    public double CalculateTotalPrice()
+    {
+        TotalPrice = Quantity * Price;
+        return TotalPrice;
+    }
+    override public string ToString()
+    {
+        return this.Fruit.Name;
+    }
+}
---
+++ FruitStand/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.IO;
+using System.Text.Json;
+
+namespace FruitStand
+{
+    internal class Program
+    {
+        const string filePath = "FruitStand.json";
+        public static void SaveJSon(Basket basket, string filePath)
+        {
+            File.Open(filePath, FileMode.OpenOrCreate).Close();
+            string json = JsonSerializer.Serialize(basket);
+        }
+        public Basket LoadFromJson(string filePath)
+        {
+            Basket basket;
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                basket = JsonSerializer.Deserialize<Basket>(sr.ReadLine());
+            }
+            return basket;
+        }
+        private static void Main(string[] args)
+        {
+            Program program = new Program();
+            Basket basket = new Basket();
+            basket = program.ask(filePath);
+            Console.WriteLine("Total price of basket: " + basket.TotalPrice);
+        }
+        public Basket ask(string filePath)
+        {
+            Basket basket = new Basket();
+            Console.Write("Y,N: ");
+            string @ask = Console.ReadLine().ToUpper();
+            while (@ask != "Y" && @ask != "N")
+            {
+                Console.Write("Y,N: ");
+                @ask = Console.ReadLine().ToUpper();
+            }
+            switch (@ask)
+            {
+                case "Y":
+                    Fruit apple = new Apple(35);
+                    basket = new Basket(apple, 5);
+                    basket.CalculateTotalPrice();
+                    break;
+                case "N":
+                    break;
+            }
+            SaveJSon(basket, filePath);
+            return basket;
+        }
+    }
+}
