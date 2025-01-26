abbd5347a92533908b4553ce270dc222de478e15
Szar-S <sezaikelekcier21@gmail.com>
2025-01-26 10:20:04 +0300

    update

---
+++ AskAndGet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+//Asks the user for input and returns the input
+public static class AskAndGet
+{
+    public static string String(string question)
+    {
+        Console.Write(question);
+        return Console.ReadLine();
+    }
+    public static int Int(string question)
+    {
+        Console.Write(question);
+        string input = Console.ReadLine();
+        int result;
+        while (!int.TryParse(input, out int value) || input.Replace(" ", "") == "")
+        {
+            Console.Write(question);
+            input = Console.ReadLine();
+        }
+        result = Convert.ToInt32(input);
+        return result;
+    }
+    public static double Double(string question)
+    {
+        Console.Write(question);
+        string input = Console.ReadLine();
+        double result;
+        while (!double.TryParse(input, out double value) || input.Replace(" ", "") == "")
+        {
+            Console.Write(question);
+            input = Console.ReadLine();
+        }
+        result = Convert.ToDouble(input);
+        return result;
+    }
+}
--- FruitStand/Program.cs
+++ FruitStand/Program.cs
@@ -10,41 +10,6 @@ namespace FruitStand
     internal class Program
     {
         const string FilePath = "FruitStand.json";
-        //Asks the user for input and returns the input
-        static class AskAndGet
-        {
-            public static string String(string question)
-            {
-                Console.Write(question);
-                return Console.ReadLine();
-            }
-            public static int Int(string question)
-            {
-                Console.Write(question);
-                string input = Console.ReadLine();
-                int result;
-                while (!int.TryParse(input, out int value) || input.Replace(" ", "") == "")
-                {
-                    Console.Write(question);
-                    input = Console.ReadLine();
-                }
-                result = Convert.ToInt32(input);
-                return result;
-            }
-            public static double Double(string question)
-            {
-                Console.Write(question);
-                string input = Console.ReadLine();
-                double result;
-                while (!double.TryParse(input, out double value) || input.Replace(" ", "") == "")
-                {
-                    Console.Write(question);
-                    input = Console.ReadLine();
-                }
-                result = Convert.ToDouble(input);
-                return result;
-            }
-        }
         private static void Main(string[] args)
         {
             bool yes = true;
